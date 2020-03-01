6aa7024f43c476545f0754270ea824342e237408
David Tavarez <david@punksec.com>
2020-03-01 09:38:01 +0100

    mmm

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-export default async function assets(coin, protocol) {
+export default async function coinbase(coin, protocol) {
   const response = await fetch(
-      `${protocol}://api.coincap.io/v2/assets/${coin}`,
-      {method: 'GET', cache: 'no-cache', redirect: 'follow'});
+    `${protocol}://api.coincap.io/v2/assets/${coin}`,
+    { method: "GET", cache: "no-cache", redirect: "follow" }
+  );
   return await response.json();
-};
+}
