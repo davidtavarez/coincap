48f83336653fc83486b679a8d9eebd05fdf4a060
David Tavarez <david@punksec.com>
2020-03-01 10:07:09 +0100

    Working version :P

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = async function coincap(coin, protocol) {
+const fetch = require("node-fetch");
+
+module.exports = async function coincap(coin, protocol = "https") {
   const response = await fetch(
     `${protocol}://api.coincap.io/v2/assets/${coin}`,
     { method: "GET", cache: "no-cache", redirect: "follow" }
